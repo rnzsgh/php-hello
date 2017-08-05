0c8af625b5a6dddcf568437a1665d6ad06b36f82
Ryan <ryannitz@amazon.com>
2017-08-04 21:06:52 -0400

    Initial

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Simple PHP App</title>
+    </head>
+
+    <body>
+        <h1>Simple PHP App</h1>
+        <h2>Congratulations</h2>
+        <p>Your PHP application is now running on a container in Amazon ECS.</p>
+        <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
+        <?php
+          $myfile = fopen("/var/www/my-vol/date", "r") or die("");
+          echo fread($myfile,filesize("/var/www/my-vol/date"));
+          fclose($myfile);
+        ?>
+    </body>
+</html>
