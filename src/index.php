cf072e740666166cc7f85b0d427eea1fdf30ef99
Ryan <ryannitz@amazon.com>
2017-08-09 20:53:58 -0400

    Layout

--- src/index.php
+++ src/index.php
@@ -2,11 +2,10 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Simple PHP App</title>
+        <title>php-hello</title>
     </head>
-
     <body>
-        <h1>Simple PHP App</h1>
+        <h1>php-hello</h1>
         <h2>Congratulations</h2>
         <p>Your PHP application is now running on a container in Amazon ECS.</p>
         <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
@@ -16,7 +15,6 @@
           echo fread($myfile,filesize("/var/www/my-vol/date"));
           fclose($myfile);
         ?>
-
         <br><br><a href="/404">Not Found</a>
     </body>
 </html>
